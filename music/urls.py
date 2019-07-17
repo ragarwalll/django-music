815891b6648f8f696639d5f9543ed4648b0626be
Rahul Agarwal <agarwal.rahul324@gmail.com>
2019-07-17 17:45:50 +0530

    Add Fav

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     path('', views.index, name='index'),
 
     # /music/{album_id}
     re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/{album_id}/fav
+    re_path(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
