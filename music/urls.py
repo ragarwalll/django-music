f94c50cbd2e5d8fca4a07439963ddfed59cb25ce
Rahul Agarwal <agarwal.rahul324@gmail.com>
2019-07-19 01:10:33 +0530

    User Registration

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,13 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    path('', views.index, name='index'),
-
+    path('', views.IndexView.as_view(), name='index'),
     # /music/{album_id}
-    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+    re_path(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+    path('register/', views.UserFormView.as_view(), name='register'),
+    path('logout/', views.UserLogout, name='logout'),
 
-    # /music/{album_id}/fav
-    re_path(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
